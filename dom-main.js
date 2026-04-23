33dc16580abf797c412e207222c484c26fb832f9
Zain Ul Abdeen <ZainUlAbdeen7391>
2026-04-24 02:25:47 +0500

    explored DOM Methods

--- dom-main.js
+++ dom-main.js
@@ -1,4 +1,30 @@
 
-const y = document.getElementsByTagName("p").innerText;
-console.log(y)
+var element;
+
+
+
+// addeventlistner
+
+
+
+document.getElementById("manu").previousElementSibling
+
+function abc(){
+    var a = document.getElementById("manu").previousElementSibling
+
+    console.log(a)
+
+}
+
+// document.getElementById("header").classList.length
+
+
+
+
+
+
+
+
+
+
 
---
+++ dom-nav.js
@@ -0,0 +1,6 @@
+// document.getElementById("child-C").nextElementSibling.style.background = "red";
+var element = document.getElementById("child-C").nextElementSibling
+
+console.log(element)
+
+
